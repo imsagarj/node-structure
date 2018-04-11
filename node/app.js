0659ccbf607d0d881af9dadf3c48a7990e761d58
Sagar <jadhavsagar698@gmail.com>
2018-04-11 17:55:56 +0530

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,12 +1,15 @@
 
 var express=require('express');
-//var bodyParser=require('body-parser');
+var bodyParser=require('body-parser');
 
 var app=express();
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:false}));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended:false}));
 
+app.get('/',function(req,res){
+	res.send("Hello Sagar Jadhav");
+});
 app.listen(7000,function(err,res){
 	console.log("Application run onn port"+" "+7000);
 });
